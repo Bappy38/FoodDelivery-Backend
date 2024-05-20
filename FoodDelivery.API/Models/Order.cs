ba98299225b3e56201f0781e7e8c4543e3de469c
iqbal_bappy <iqbal.hossain@orbitax.com>
2024-05-20 18:39:52 +0600

    OrderRepository Added

--- FoodDelivery.API/Models/Order.cs
+++ FoodDelivery.API/Models/Order.cs
@@ -6,6 +6,7 @@ public class Order
 {
     public int Id { get; set; }
     public int UserId { get; set; }
+    public int RestaurantId { get; set; }
     public List<CartItem> Items { get; set; }
     public string DeliveryAddress { get; set; }
     public string PaymentMethod { get; set; }
---
+++ FoodDelivery.API/Repositories/OrderRepository.cs
@@ -0,0 +1,58 @@
+﻿using FoodDelivery.API.Models;
+
+namespace FoodDelivery.API.Repositories;
+
+public interface IOrderRepository
+{
+    bool Add(Order order);
+    bool Update(Order order);
+    Order GetById(int id);
+    List<Order> GetOrdersByRestaurantId(int restaurantId);
+    List<Order> GetOrdersByUserId(int userId);
+}
+
+public class OrderRepository : IOrderRepository
+{
+    private static List<Order> _orders;
+
+    static OrderRepository()
+    {
+        _orders = new List<Order>();
+    }
+
+    public bool Add(Order order)
+    {
+        _orders.Add(order);
+        return true;
+    }
+
+    public bool Update(Order order)
+    {
+        var existingOrder = _orders.FirstOrDefault(o => o.Id == order.Id);
+        if (existingOrder == null)
+        {
+            return false;
+        }
+
+        _orders.Remove(existingOrder);
+        _orders.Add(order);
+        return true;
+    }
+
+    public Order GetById(int id)
+    {
+        return _orders.FirstOrDefault(o => o.Id == id);
+    }
+
+    public List<Order> GetOrdersByRestaurantId(int restaurantId)
+    {
+        var orders = _orders.Where(o => o.RestaurantId == restaurantId).ToList();
+        return orders;
+    }
+
+    public List<Order> GetOrdersByUserId(int userId)
+    {
+        var orders = _orders.Where(o => o.UserId == userId).ToList();
+        return orders;
+    }
+}
